cc8fa4abbbc1adcb23bc18902b30be8a008d8d52
jhlee2729 <wkdgml7644@gmail.com>
2023-04-19 11:26:41 +0900

    코드 수정 1. code 2. getAccessToken

---
+++ 1. code.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const btoa = require('btoa');
+const axios = require('axios');
+
+// code 발급
+const getCodeURL = () => {
+
+    const scope = 'mall.read_application, mall.read_product, mall.write_product, mall.read_category,mall.write_category, mall.read_customer,mall.write_customer, mall.read_collection,mall.write_collection'
+    const url = `https://${process.env.ADMIN_ID}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&scope=${scope}`
+    return url;
+
+}
+
+console.log(getCodeURL());
